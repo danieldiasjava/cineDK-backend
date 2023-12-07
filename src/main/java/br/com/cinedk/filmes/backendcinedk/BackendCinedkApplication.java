9de01bff9620ee70c735bf89950817919fca850e
Daniel Dias <danieldias.devjava@gmail.com>
2023-12-07 09:14:32 -0300

    Project created

---
+++ src/main/java/br/com/cinedk/filmes/backendcinedk/BackendCinedkApplication.java
@@ -0,0 +1,13 @@
+package br.com.cinedk.filmes.backendcinedk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCinedkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCinedkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cinedk/filmes/backendcinedk/BackendCinedkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cinedk.filmes.backendcinedk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCinedkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
